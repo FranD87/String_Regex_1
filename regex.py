35581da767513cf065e3b37fc2469f564b950bfb
FranD87 <fdunne007@gmail.com>
2023-01-17 12:44:37 +0100

    Commit

---
+++ regex.py
@@ -0,0 +1,49 @@
+# Task 1
+
+import re
+
+# text = 'Berlin is a world city of culture, politics, media and science.'
+#
+# result = re.search('\\s', text)
+#
+# print("The first white-space character is located at position:", result)
+
+# Task 2
+
+# text = "Berlin is surrounded by the state Brandenburg and contiguous with Potsdam, Brandenburg's capital."
+#
+# result = re.search('Frankfurt', text)
+#
+# print(result)
+
+# Task 3
+
+# text = "Berlin is a city of culture."
+#
+# result = re.sub(' ', '-', text)
+#
+# print(result)
+
+# Task 4
+
+# text = "Berlin is a city of culture"
+#
+# result = re.search('in', text)
+#
+# print(result)
+
+# Task 5
+#
+# text = "Berlin is a city of culture"
+#
+# result = re.search('B', text)
+#
+# print(result.start(), result.end())
+
+# Task 6
+
+# text = "The rain in Spain"
+#
+# result = re.findall('ai', text)
+#
+# print(len(result))
